9af5a6783021f0b769d21f3a7fa8f95890b69094
rud-y <rudolf.zachar@gmail.com>
2021-07-22 16:41:13 +0100

    elements layot+ pressOn/pressOff functions

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const score = document.querySelector(".score");
+  const startScreen = document.querySelector(".start-screen");
+  const gameArea = document.querySelector(".game-area");
+
+  startScreen.addEventListener("click", start);
+  document.addEventListener("keydown", pressOn);
+  document.addEventListener("keydown", pressOff);
+
+  function pressOn(e) {
+    e.preventDefault();
+    console.log("ON", e.key);
+  }
+
+  function pressOff(e) {
+    e.preventDefault();
+    console.log("OFF", e.key);
+  }
+
+  function start() {
+    console.log("you clicked screen");
+  }
+});
